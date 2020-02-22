b899c34fa52cca82d72adb0b29aed2f47f78059f
John Cassidy <jpcassidy@outlook.com>
2020-02-21 19:04:32 -0500

    Create core, message, and model Modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'paRoot',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
